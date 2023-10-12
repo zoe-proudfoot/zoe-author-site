444d8c1973140336df1b87b25aab7c96af819ebc
Zoe Proudfoot <zoeannproudfoot123@gmail.com>
2023-10-12 16:14:40 +1300

    Ugggh

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ document.getElementById('contact').addEventListener('submit', function () {
     document.getElementById('contact').reset()
   }, 10)
 })
+
+// document.getElementById('arc-signup').addEventListener('submit', function () {
+//   setTimeout(function () {
+//     document.getElementById('arc-signup').reset()
+//   }, 10)
+// })
+
+// document.getElementById('arc-signup').addEventListener('submit', function () {
+//   setTimeout(function () {
+//     document.getElementById('arc-signup').reset()
+//   }, 10)
+// })
